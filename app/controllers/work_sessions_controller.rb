aab070ecdff840f8a119f2c2d569602a5a4f9615
Olivier <orohellec44@gmail.com>
2019-02-20 15:51:44 +0100

    worker_session view form created

---
+++ app/controllers/work_sessions_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class WorkSessionsController < ApplicationController
+  def new
+    @work_session = WorkSession.new
+    @skills = Skill.all
+    @slots = Slot.all
+    @teachers = Teacher.all
+    @students = Student.all
+  end
+
+  def create
+    redirect_to root_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+#  root to: 'work_sessions/index'
   resources :teachers, only: %i[index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :students, only: [:index]
+  resources :work_sessions, only: [:new, :create]
 end
