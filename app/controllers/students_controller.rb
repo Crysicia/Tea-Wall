a30d112e0dec6a6d640e29e26a13f76e63f68997
Alexisreau97 <alexis.reaupro@gmail.com>
2019-02-20 14:05:44 +0100

    add edit and destroy method to student

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,4 +4,31 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    if @student.update(student_params)
+      redirect_to action: 'index'
+      flash[:success] = "Modification enregistrée"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @student = Student.find(params[:id])
+    @student.destroy
+    flash[:success] = "Élève supprimé"
+    redirect_to students_path
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:first_name, :last_name)
+  end
 end
