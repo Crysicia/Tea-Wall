3a61cdb5147ea46634a13244febd50408626568a
Alexisreau97 <alexis.reaupro@gmail.com>
2019-02-21 12:23:31 +0100

    add edit method to worksession

--- app/controllers/work_sessions_controller.rb
+++ app/controllers/work_sessions_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class WorkSessionsController < ApplicationController
+  before_action :find_work_session, only: %i[edit update]
+
   def new
     @work_session = WorkSession.new
     @skills = Skill.all
@@ -18,6 +20,22 @@ class WorkSessionsController < ApplicationController
     @work_sessions = WorkSession.all.order(:date)
   end
 
+  def edit
+    @skills = Skill.all
+    @slots = Slot.all
+    @teachers = Teacher.all
+    @students = Student.all
+  end
+
+  def update
+    if @work_session.update(ws_parameters)
+      flash[:success] = "Modification enregistrée"
+      redirect_to action: 'index'
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def ws_parameters
@@ -25,8 +43,13 @@ class WorkSessionsController < ApplicationController
       :title,
       :date,
       :slot_id,
+      skill_ids: [],
       teacher_ids: [],
       student_ids: []
     )
+  end
+
+  def find_work_session
+    @work_session = WorkSession.find(params[:id])
   end
 end
