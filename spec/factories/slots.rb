ad2c7607a8bd59276c311bc627f6e01859ef7722
Crysicia <lucienmontmartre@yahoo.fr>
2019-02-22 20:29:05 +0100

    Fix slot factory

--- spec/factories/slots.rb
+++ spec/factories/slots.rb
@@ -15,7 +15,7 @@
 FactoryBot.define do
   factory :slot do
     title { Faker::DcComics.hero }
-    start_time { Faker::Time.between(Time.zone.now - 5.hours, Time.zone.now) }
+    start_time { Faker::Time.between(Time.zone.now - 5.hours, Time.zone.now - 1.hour) }
     end_time { Faker::Time.between(Time.zone.now + 1.hour, Time.zone.now + 4.hours) }
   end
 end
