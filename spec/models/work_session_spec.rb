70445aa7b82e0be625f8cc3952a40df25d70923e
Crysicia <lucienmontmartre@yahoo.fr>
2019-02-22 09:55:58 +0100

    Annotate update

--- spec/models/work_session_spec.rb
+++ spec/models/work_session_spec.rb
@@ -6,7 +6,7 @@
 #
 #  id         :bigint(8)        not null, primary key
 #  title      :string
-#  date       :datetime
+#  date       :date
 #  slot_id    :bigint(8)
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
