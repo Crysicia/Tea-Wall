854e08f1bdbe9b053eb9a9409ab108c4d9e20edb
Alexisreau97 <alexis.reaupro@gmail.com>
2019-02-25 14:49:36 +0100

    add test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -16,5 +16,7 @@
 
 FactoryBot.define do
   factory :user do
+    email { Faker::Internet.email }
+    password { Faker::Internet.password }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,5 +17,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:new_user) { described_class.new }
+
+  let(:user) { create(:user) }
+
+  describe 'Database' do
+    it { is_expected.to have_db_column(:email).of_type(:string).with_options(default: '', null: false) }
+    it { is_expected.to have_db_column(:encrypted_password).of_type(:string).with_options(default: '', null: false) }
+    it { is_expected.to have_db_column(:reset_password_token).of_type(:string) }
+    it { is_expected.to have_db_column(:reset_password_sent_at).of_type(:datetime) }
+    it { is_expected.to have_db_column(:remember_created_at).of_type(:datetime) }
+    it { is_expected.to have_db_column(:created_at).of_type(:datetime).with_options(null: false) }
+    it { is_expected.to have_db_column(:updated_at).of_type(:datetime).with_options(null: false) }
+  end
+
+  describe 'validation' do
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:password) }
+  end
 end
