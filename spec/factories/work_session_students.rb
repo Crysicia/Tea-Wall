69b620e25d0c8da9c30b60c9cf8296fdc1d26885
Alexisreau97 <alexis.reaupro@gmail.com>
2019-02-25 12:31:24 +0100

    translate devise message

--- spec/factories/work_session_students.rb
+++ spec/factories/work_session_students.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-
 # == Schema Information
 #
 # Table name: work_session_students
@@ -9,6 +8,7 @@
 #  student_id      :bigint(8)
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
+#  present         :boolean          default(FALSE)
 #
 
 FactoryBot.define do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,20 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint(8)        not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
