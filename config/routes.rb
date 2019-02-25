d2ca3aee463ee8ec18abc56c773d361c227662fb
Alexisreau97 <alexis.reaupro@gmail.com>
2019-02-25 12:02:36 +0100

    setup devise

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'work_sessions#index'
+  devise_for :users
   resources :students
   resources :teachers
   resources :work_sessions, path: 'sessions' do
